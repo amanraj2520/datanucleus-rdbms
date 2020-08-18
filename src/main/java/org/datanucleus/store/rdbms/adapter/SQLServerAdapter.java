270cc502cc380b0c8c747031717a9e1c0bf1aef4
wenjunma004 <wenjunma004@gmail.com>
2020-08-18 08:08:02 +0530

    add LOCK_WITH_SELECT_WITH_UPDLOCK to solve duplicated id issue for sql server
    (cherry picked from commit 37b29d60144dde3e7f39f84b52281125691e93f0)

--- src/main/java/org/datanucleus/store/rdbms/adapter/SQLServerAdapter.java
+++ src/main/java/org/datanucleus/store/rdbms/adapter/SQLServerAdapter.java
@@ -117,7 +117,7 @@ public class SQLServerAdapter extends BaseDatastoreAdapter
         supportedOptions.add(LOCK_ROW_USING_OPTION_WITHIN_JOIN);
         supportedOptions.add(STORED_PROCEDURES);
         supportedOptions.add(ORDERBY_NULLS_USING_CASE_NULL);
-        supportedOptions.add(LOCK_WITH_SELECT_FOR_UPDATE);
+        supportedOptions.add(LOCK_WITH_SELECT_WITH_UPDLOCK);
 
         supportedOptions.remove(BOOLEAN_COMPARISON);
         supportedOptions.remove(DEFERRED_CONSTRAINTS);
